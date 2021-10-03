12a92474dbd9a3381edca121111812a65eb9ebd0
Kyonax <kyonax25@gmail.com>
2021-10-03 15:53:17 -0500

    Agregamos un archivo de prueba

---
+++ test/Cristian_Moreno/src/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+namespace src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Customer customer = new Customer();
+        }
+    }
+    public class Customer
+    {
+        public string FirstName {get;set;}
+        public string LastName {get;set;}
+    }
+}
