2da4ed5be47fa4e74fbe4b3f816cf5f6e6deccbc
unknown <juandiego98.rm@gmail.com>
2021-10-16 15:41:02 -0500

    Update

--- src/Ganaderia.App/Ganaderia.App.Dominio/Entidades/Ganadero.cs
+++
@@ -1,8 +0,0 @@
-namespace Ganaderia.App.Dominio
-{
-    public class Ganadero : User 
-    {
-        public float Latitud { get; set; }
-        public float Longitud { get; set; }
-    }
-}
--- src/Ganaderia.App/Ganaderia.App.Persistencia/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Ganaderia.App.Persistencia
-{
-    public class Class1
-    {
-    }
-}
